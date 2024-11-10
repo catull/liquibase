830f1f7831acf000c3f53bbac855dca875e76207
Carlo Dapor <carlo.dapor@mimacom.com>
2024-11-10 23:37:03 +0100

    W.I.P

--- liquibase-standard/src/main/java/liquibase/sqlgenerator/core/InsertGenerator.java
+++ liquibase-standard/src/main/java/liquibase/sqlgenerator/core/InsertGenerator.java
@@ -83,8 +83,10 @@ public class InsertGenerator extends AbstractSqlGenerator<InsertStatement> {
 
         for (String column : statement.getColumnValues().keySet()) {
             Object newValue = statement.getColumnValues().get(column);
-            if ((newValue == null) || "NULL".equalsIgnoreCase(newValue.toString())) {
+            if (newValue == null) {
                 sql.append("NULL");
+            } else if ("NULL".equalsIgnoreCase(newValue.toString())) {
+                sql.append("'").append(newValue).append("'");
             } else if ((newValue instanceof String) && !looksLikeFunctionCall(((String) newValue), database)) {
                 sql.append(DataTypeFactory.getInstance().fromObject(newValue, database).objectToSql(newValue, database));
             } else if (newValue instanceof Date) {
